585cf28600be85a6c4e8d2adaf38f60406e69dd6
Sriharsha Sistalam <sriharsha@moengage.com>
2020-07-17 08:08:28 +0530

    Support for cherry picking merge commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,6 +36,6 @@ git_cmd git remote update
 git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
 git_cmd git push -u origin "${PR_BRANCH}"
-git_cmd git cherry-pick "${GITHUB_SHA}"
+git_cmd git cherry-pick "${GITHUB_SHA}" -m 1
 git_cmd git push
 git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${MESSAGE}\""
